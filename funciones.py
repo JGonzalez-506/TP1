4bd8de9200299ac2cc167795003bec4c3f785b96
Juan Gonzalez <JGonzalez506@protonmail.com>
2026-04-30 11:47:25 -0600

    Se agrega funcion de Mostrar Tokens

--- funciones.py
+++ funciones.py
@@ -66,3 +66,12 @@ def agregrarModificarTokens(separador,tokens,nuevosTokens):
     if aceptar == 1:
         return tokens
     return tokensAntiguos
+#Mostrar tokens
+def mostrarTokens(tokens):
+    if len(tokens) == 0:
+        print("No hay tokens cargados.")
+    else :
+        print("Tokens Cargados hasta el momento:")
+        for original, equivalencia in tokens:
+            print(f"{original} -> {equivalencia}")
+    return ""
