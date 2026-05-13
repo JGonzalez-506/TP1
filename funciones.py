afa9d0ee6b7d3e73534d9aa9005a63eb5ea5012c
Alessandro Arias <alessandroariasalfaro@gmail.com>
2026-05-12 18:38:53 -0600

    V0.5

--- funciones.py
+++ funciones.py
@@ -5,6 +5,9 @@
 
 #Definición de funciones
 #Cargar tokens
+import datetime
+import pickle
+import csv
 def validarToken(token):
     import re
     if re.match('[A-Za-z]', token[0] and token[1]):
@@ -39,7 +42,7 @@ def cargarTokens(nombreArchivo,separador,tokens):
     archivo.close()
     return tokens
 #Agregar o modificar tokens
-def agregrarModificarTokens(separador,tokens,nuevosTokens):
+def agregarModificarTokens(separador,tokens,nuevosTokens,bitacora):
     tokensAntiguos = tokens.copy()
     separador = escogerSeparador(separador)
     nuevosTokens = nuevosTokens.split(".")
@@ -52,10 +55,12 @@ def agregrarModificarTokens(separador,tokens,nuevosTokens):
             for indice, pos in enumerate(tokens):  # Lee cada equivalencia ya guardada en la lista de tokens
                 if pos[0] == token[0]:              # Si el nuevo token ya existe, imprime que se va a reescribir
                     print(f"Se reescribió el token de '{token[0]}'")
+                    insertarBitacora(bitacora, f"Se modificó el token {token[0]}")
                     tokens[indice] = token
                     repetido = True
             if not repetido:
                 print(f"Se añadió el token de '{token}'")
+                insertarBitacora(bitacora, f"Se añadió el token {token[0]}")
                 tokens.append(token)
         else:
             print(f"El token '{token}' no es válido")
@@ -69,92 +74,110 @@ def agregrarModificarTokens(separador,tokens,nuevosTokens):
 #Mostrar tokens
 def mostrarTokens(tokens):
     if len(tokens) == 0:
+
         print("No hay tokens cargados.")
+        return ""
     else:
-        print("Tokens Cargados hasta el momento:")
+        print("Tokens Cargados hasta el momento separados por ->:")
         for original, equivalencia in tokens:
             print(f"{original} -> {equivalencia}")
     return ""
-#Traducir código
-def validarTokens(llaves, tokens):
-    temp = []
-    for x in llaves:
-        for y in tokens:
-            if x[0] == y[0]:
-                temp.append(x)
-    if len(temp) > 0:
-        return temp
-    return False
-def cambiarToken(token,tokens):
-    for x in tokens:
-        if x[0] == token:
-            token = x[1]
-            break
-    return token
-def contarReemplazos(reemplazos, llaves):
-    for x in llaves:
-        if reemplazos != []:
-            repetido = False
-            for y in reemplazos:
-                if y[0] == x[0]:
-                    y[1] += 1
-                    repetido = True
-            if repetido == False:
-                reemplazos.append([x[0], 1])
-        else:
-            reemplazos.append([x[0], 1])
-    return reemplazos
-def traducirLinea(line, llaves, tokens):
-    indice = 0
-    primera = True
-    traduccion = ""
-    for x in range(len(llaves)):
-        cambioAct = llaves[indice]
-        token = cambioAct[0]
-        equivalencia = cambiarToken(token,tokens)
-        inicio = cambioAct[1]
-        fin = cambioAct[2]
-        if indice < len(llaves)-1:
-            cambioSig = llaves[indice + 1]
-            sig = cambioSig[1]
-            indice += 1
-        else:
-            sig = fin
-        if primera:
-            traduccion += line[:inicio] + equivalencia + line[fin:sig]
-            primera = False
-        else:
-            traduccion += equivalencia + line[fin:sig]
-    traduccion += line[fin:len(line)]
-    return traduccion
-def traducirCodigo(archivo, nArchivo, tokens):
-    import re
+def guardarTokens(tokens):
+    """
+    Funcionamiento: Guarda los tokens en un archivo de texto utilizando un separador seleccionado por el usuario.
+    Entradas: tokens (list).
+    Salidas: str (retorna cadena vacía).
+    """
+    if len(tokens) == 0:
+        print("No hay tokens para guardar.")
+        return
+    nombreArchivo = input("Indique el nombre del nuevo archivo donde desea guardar los tokens: ")
+    separador = int(input("Indique el separador que desea usar:\n1-'->'\n2-','\n3-'='\nOpción: "))
+    while separador not in [1,2,3]:
+        separador = int(input("Indique el separador que desea usar:\n1-'->'\n2-','\n3-'='\nOpción: "))
+    separador = escogerSeparador(separador)
+    with open(f"{nombreArchivo}.txt", "w") as archivo:
+        for original, equivalencia in tokens:
+            archivo.write(f"{original} {separador} {equivalencia}\n")
+    print("Tokens guardados correctamente.")
+    return ""
+def generarCSV():
+    """
+    Funcionamiento: Permite al usuario ingresar datos de reemplazos y genera un archivo CSV con esa información.
+    Entradas: No recibe parámetros (los datos se solicitan al usuario mediante input).
+    Salidas: None (crea un archivo .csv con los datos ingresados).
+    """
     reemplazos = []
-    llaves = []
-    f = open(archivo, "r")
-    g = open(nArchivo, "a")
-    for line in f:
-        for llave in re.finditer(r"[A-Za-z0-9]+", line):
-            temp = []
-            temp.append(llave.group())
-            temp.append(llave.start())
-            temp.append(llave.end())
-            llaves.append(temp)
-        llaves = validarTokens(llaves, tokens)
-        if llaves == False:
-            print("No hay reemplazos por hacer.")
-            g.write(line)
-        else:
-            reemplazos = contarReemplazos(reemplazos, llaves)  # Cuenta las veces que se cambia una palabra
-            traduccion = traducirLinea(line, llaves, tokens)
-            g.write(traduccion)
-        llaves = []
-    f.close()
-    g.close()
-    return reemplazos
-def traducirCodigoAux(tokens):
-    archivo = input("Introduzca el archivo a traducir, indique la extensión: ")
-    nArchivo = input("Introduzca el nombre del archivo donde desea guardar la traducción, no escriba ninguna extensión: ")
-    nArchivo += ".txt"
-    reemplazos = traducirCodigo(archivo, nArchivo, tokens)
-    return reemplazos
+    try:
+        cantidad = int(input("¿Cuántos reemplazos desea ingresar? "))
+    except ValueError:
+        print("Cantidad inválida")
+        return
+    for i in range(cantidad):
+        original = input("Digite la palabra original: ")
+        reemplazo = input("Digite el reemplazo: ")
+        try:
+            veces = int(input("Digite la cantidad de reemplazos realizados: "))
+        except ValueError:
+            print("Cantidad inválida")
+            return
+        reemplazos.append((original, reemplazo, veces))
+    nombreArchivo = input("Digite el nombre del archivo CSV: ")
+    with open(f"{nombreArchivo}.csv", "w", newline="", encoding="utf-8") as archivoCSV: # Abre el archivo CSV para escritura y asegura que se maneje bien el salto de línea y los caracteres diferentes
+        escritor = csv.writer(archivoCSV)
+        escritor.writerow(["Palabra Original", "Reemplazo", "Cantidad"])
+        for original, reemplazo, cantidad in reemplazos:
+            escritor.writerow([original, reemplazo, cantidad])
+    print("Reporte CSV generado correctamente.")
+def insertarBitacora(bitacora, descripcion):
+    """
+    Funcionamiento: Agrega un nuevo registro a la bitácora con fecha y hora actual y guarda los datos en un archivo.
+    Entradas: bitacora (list), descripcion (str).
+    Salidas: None.
+    """
+    fechaHora = datetime.datetime.now()
+    fechaHora = fechaHora.strftime("%Y-%m-%d_%H:%M:%S")
+    registro = (fechaHora, descripcion)
+    bitacora.append(registro)
+    with open("bitacora.txt", "ab") as archivo:
+        pickle.dump(bitacora, archivo)
+def cargarBitacora():
+    """
+    Funcionamiento: Carga la bitácora desde un archivo si existe, o crea una lista vacía en caso contrario.
+    Entradas: Ninguna.
+    Salidas: list (bitacora).
+    """
+    try:
+        with open("bitacora.txt", "rb") as archivo:
+            bitacora = pickle.load(archivo)
+    except:
+        bitacora = []
+    return bitacora
+def filtrarPorDia(bitacora):
+    """
+    Funcionamiento: Muestra los registros de la bitácora que coinciden con una fecha específica ingresada por el usuario.
+    Entradas: bitacora (list).
+    Salidas: None.
+    """
+    fecha = input("Digite la fecha a buscar (AAAA-MM-DD): ")
+    encontrados = False
+    for fechaHora, descripcion in bitacora:
+        if fechaHora.startswith(fecha):
+            print(f"{fechaHora} -> {descripcion}")
+            encontrados = True
+    if not encontrados:
+        print("No se encontraron registros para esa fecha.")
+def filtrarPorPalabra(bitacora):
+    """
+    Funcionamiento: Muestra los registros de la bitácora que contienen una palabra clave ingresada por el usuario.
+    Entradas: bitacora (list).
+    Salidas: None.
+    """
+    palabra = input("Digite la palabra clave a buscar: ").lower()
+    encontrados = False
+    for fechaHora, descripcion in bitacora:
+        if palabra in descripcion.lower():
+            print(f"{fechaHora} -> {descripcion}")
+            encontrados = True
+    if not encontrados:
+        print("No se encontraron coincidencias.")
--- main.py
+++ main.py
@@ -5,22 +5,27 @@
 
 #Importaciones
 import funciones
-
+reemplazos = []
 #Definición de funciones
-def submenu(opt,tokens):
+def submenu(opt,tokens,bitacora):
     if opt == 1:
-        return "A"
+        funciones.filtrarPorDia(bitacora)
+        funciones.insertarBitacora(bitacora, "Se buscó en bitácora por fecha")
     elif opt == 2:
-        return "B"
-def menu(opt,tokens):
+        funciones.filtrarPorPalabra(bitacora)
+        funciones.insertarBitacora(bitacora, "Se buscó en bitácora por palabra clave")
+    return ""
+def menu(opt,tokens,bitacora):
     if opt == 1:
         nombreArchivo = input("Indique el nombre del archivo donde están almacenados los tokens, no escriba la extención del archivo: ")
         separador = int(input("Indique el separador utilizado:\n1-'->'\n2-','\n3-'='\nOpción: "))
         while separador not in [1,2,3]:
             separador = int(input("Indique el separador utilizado:\n1-'->'\n2-','\n3-'='"))
         tokens = funciones.cargarTokens(nombreArchivo,separador,tokens)
+        funciones.insertarBitacora(bitacora, "Se cargó un archivo de tokens")
         return tokens
     elif opt == 2:
+        funciones.insertarBitacora(bitacora, "Se mostraron los tokens")
         return funciones.mostrarTokens(tokens)
     elif opt == 3:
         nuevosTokens = input("Porfavor ingrese los nuevos tokens, utilize un punto '.' para separar cada token, y utilize un separador"
@@ -28,33 +33,38 @@ def menu(opt,tokens):
         separador = int(input("Indique el separador utilizado:\n1-'->'\n2-','\n3-'='\nOpción: "))
         while separador not in [1, 2, 3]:
             separador = int(input("Indique el separador utilizado:\n1-'->'\n2-','\n3-'='"))
-        tokens = funciones.agregrarModificarTokens(separador, tokens, nuevosTokens)
+        tokens = funciones.agregarModificarTokens(separador, tokens, nuevosTokens,bitacora)
         return tokens
     elif opt == 4:
-        return "4"
+            funciones.guardarTokens(tokens)
+            funciones.insertarBitacora(bitacora, "Se guardaron tokens en un archivo")
+            return ""
     elif opt == 5:
         reemplazos = funciones.traducirCodigoAux(tokens)
         return "Traducción finalizada"
     elif opt == 6:
-        return "6"
+        funciones.generarCSV()
+        funciones.insertarBitacora(bitacora, "Se generó un archivo CSV")
     elif opt == 7:
         return "7"
     elif opt == 8:
         opt = int(input("\nSubmenú del sistema:\n1-Acciones por día escogido\n2-Acciones con algunas palabras clave\n3-Salir\n"
                   "Porfavor digíte el número de la acción que desea realizar: "))
         while opt != 3:
-            print(submenu(opt))
+            print(submenu(opt, tokens, bitacora))
             opt = int(input("\nSubmenú del sistema:\n1-Acciones por día escogido\n2-Acciones con algunas palabras clave\n3-Salir\n"
                 "Porfavor digíte el número de la acción que desea realizar: "))
         return "8"
 #Programa Principal
 tokens = []
-opt = int(input("Menú del sistema:\n1-Cargar tokens\n2-Mostar tokens\n3-Agregar/modificar tokens\n4-Guardar tokens\n"
+bitacora = funciones.cargarBitacora()
+opt = int(input("Menú del sistema:\n1-Cargar tokens\n2-Mostrar tokens\n3-Agregar/modificar tokens\n4-Guardar tokens\n"
             "5-Traducir código\n6-Generar CSV\n7-Generar HTML\n8-Submenú de bitácora del sistema\n9-Salir\n"
             "Porfavor digíte el número de la acción que desea realizar: "))
 while opt != 9:
-    print(menu(opt,tokens))
+    print(menu(opt,tokens,bitacora))
     opt = int(input("\nMenú del sistema:\n1-Cargar tokens\n2-Mostar tokens\n3-Agregar/modificar tokens\n4-Guardar tokens\n"
           "5-Traducir código\n6-Generar CSV\n7-Generar HTML\n8-Submenú de bitácora del sistema\n9-Salir\n"
           "Porfavor digíte el número de la acción que desea realizar: "))
+funciones.insertarBitacora(bitacora, "El usuario salió del programa")
 print("Programa Finalizado")
